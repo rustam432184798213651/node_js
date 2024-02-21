df6107c212028da51404574dda9ac59ff5dcfd53
Radzhabov Rustam <rrradzhabov@edu.hse.ru>
2024-02-21 15:34:53 +0300

    Implement basic request processing

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const http = require('http');
 
+const PORT = 443;
+
 const server = http.createServer((req, res) => {
     console.log('Server request');
+    console.log(req.url, req.method);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<h1>Hello world!</h1>');
+    res.write('<h2>Something</h2>');
+    res.write('<head></head>');
+    res.end();
 });
 
-server.listen(443, 'localhost', (error) =>{
-    error ? console.log(error) : console.log('listening port 3000');
+server.listen(PORT, 'localhost', (error) =>{
+    error ? console.log(error) : console.log(`listening port ${PORT}`);
 })
